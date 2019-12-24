b6ff02658bc9738538d6821d066634f0f0f2f0cf
zlucia <40370937+zlucia@users.noreply.github.com>
2019-12-24 01:45:22 -0800

    Update pongStarterCode.py

--- python-curriculum/processing_pong/pongStarterCode.py
+++ python-curriculum/processing_pong/pongStarterCode.py
@@ -9,6 +9,7 @@ yPaddle = 225
 paddleSpeed = 5
 ballHeight = 20
 ballWidth = 20
+inGame = False
 
 def setup():
   size(600, 600)
@@ -16,26 +17,35 @@ def setup():
 def draw():
     global xPos, yPos
     global yPaddleOne
-    background(0,0,0)
+    background(0, 0, 0)
     ellipse(xPos,yPos,ballWidth,ballHeight)
     rect(xPaddle,yPaddle, paddleWidth,paddleHeight)
-    paddleMove()
-    ballMove()
+    
+    if keyPressed and keyCode == SHIFT:
+        inGame = True
+    
+    if inGame == True:
+        paddleMove()
+        paddleCollision()
+        wallCollision()
+        ballMove()
 
 def ballMove(): 
+    global xPos, yPos
     pass       
 
 def paddleMove():
+    global yPaddle
     pass
       
 def wallCollision():
+    global speedX, speedY
     pass
     
 def paddleCollision():
+    global speedX
     pass
 
 def resetGame(): 
+    global xPos, yPos, speedX, speedY, xPaddle, yPaddle, inGame
     pass
-    
-        
-    
